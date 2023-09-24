d2e14d250ffd1b83df3f8a143e6d23366564f8e5
AminKei <moakeimasi@gmail.com>
2023-09-24 16:10:45 +0330

    add appbar componnet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import Home from './Pages/Home/Home';
+import AppBar from './Components/Appbar/Appbar';
 
 function App() {
   return (
     <div className="App">
+        <AppBar/>
         <Home/>
     </div>
   );
