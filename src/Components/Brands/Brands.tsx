7b3f46773a8016a9578950ead671166dac5c7886
AminKei <moakeimasi@gmail.com>
2023-10-05 18:59:51 +0330

    brands

---
+++ src/Components/Brands/Brands.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Brands.css";
+import brands from "../../Assets/Images/brands.png";
+
+const Brands = () => {
+  return (
+    <div className="root-brands">
+      <h1>Brands</h1>
+    </div>
+  );
+};
+export default Brands;
